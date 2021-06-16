be32fac4307fd63d7ca683eb9727e0981dcef7da
melisademirparmak <melisademirparmaak@gmail.com>
2021-06-16 17:53:39 +0300

    imported TextContextProvider

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import BookList from './BookList';
 import BookContextProvider from '../contexts/BookContext';
+import ThemeContextProvider from '../contexts/ThemeContext';
 class App extends React.Component {
   render() {
     return (
-      <div>
+      <ThemeContextProvider>
         <BookContextProvider>
           <BookList />
         </BookContextProvider>
-      </div>
+      </ThemeContextProvider>
     );
   }
 }
--- src/contexts/ThemeContext.js
+++ src/contexts/ThemeContext.js
@@ -3,11 +3,15 @@ import React from 'react';
 export const ThemeContext = React.createElement();
 
 class ThemeContextProvider extends React.Component {
-  state = {};
+  state = {
+    isDarkTheme: true,
+    dark: { bg: '#222529', txt: '#d65f5f', hover: 'rgba(231,76,60,0.9)' },
+    light: { bg: '#f8f9fa', txt: '#222529', hover: 'rgba(254,209,54,0.8)' },
+  };
 
   render() {
     return (
-      <ThemeContext value={this.state}>{this.props.children}</ThemeContext>
+      <ThemeContext value={{...this.state}}>{this.props.children}</ThemeContext>
     );
   }
 }
