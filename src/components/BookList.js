4d0432e64eee14dedef4db036501b429c8a03475
melisademirparmak <melisademirparmaak@gmail.com>
2021-06-16 17:28:08 +0300

    consumer translated

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,31 +1,34 @@
 import React from 'react';
 import Book from './Book';
-import BookContext from '../contexts/BookContext';
+import { BookContext } from '../contexts/BookContext'; //because we can't get it by default {}
 import '../css/style.css';
 
 class BookList extends React.Component {
-  static contextType = BookContext;
-
   render() {
-    const books = this.context;
-
     return (
-      <section className="page-section bg-light" id="portfolio">
-        <div className="container">
-          <div className="text-center">
-            <h2 className="section-heading text-uppercase">BookFolio</h2>
-            <h3 className="section-subheading text-muted">
-              Lorem ipsum dolor sit amet consectetur.
-            </h3>
-          </div>
-          <div className="row">
-            {books.map((book, i) => {
-              return <Book book={book} key={i} />;
-            })}
-            ;
-          </div>
-        </div>
-      </section>
+      <BookContext.Consumer>
+        {(value) => {
+          //value= this.state
+          return (
+            <section className="page-section bg-light" id="portfolio">
+              <div className="container">
+                <div className="text-center">
+                  <h2 className="section-heading text-uppercase">BookFolio</h2>
+                  <h3 className="section-subheading text-muted">
+                    Lorem ipsum dolor sit amet consectetur.
+                  </h3>
+                </div>
+                <div className="row">
+                  {value.books.map((book, i) => {
+                    return <Book book={book} key={i} />;
+                  })}
+                  ;
+                </div>
+              </div>
+            </section>
+          );
+        }}
+      </BookContext.Consumer>
     );
   }
 }
